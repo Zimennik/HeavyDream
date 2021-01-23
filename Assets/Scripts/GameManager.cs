4fc5d0185ddee5bc99903a1273f14138dadc0c09
Zimennik <zimennik@gmail.com>
2021-01-23 20:06:41 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private HeavyController _heavyController;
+    [SerializeField] private PlayerController _playerController;
+
+
+    public enum GameState
+    {
+        Prepare,
+        Game,
+        GameOver,
+        Victory
+    }
+
+    private GameState _currentState = GameState.Prepare;
+
+    public void ChangeState(GameState newState)
+    {
+        switch (newState)
+        {
+            case GameState.Prepare:
+                _heavyController.Reset();
+                _playerController.Reset();
+                break;
+            case GameState.Game:
+                _heavyController.StartGame();
+                _playerController.StartGame();
+                break;
+            case GameState.GameOver:
+                _playerController.GameOver();
+                break;
+            case GameState.Victory:
+                _playerController.Victory();
+                _heavyController.StopGame();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+    }
+}
---
+++ Assets/Scripts/HeavyController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeavyController : MonoBehaviour
+{
+    [SerializeField] private Transform _targetTransform;
+    [SerializeField] private float _speed = 10;
+
+    [SerializeField] private AudioSource _audioSource;
+
+    private Camera _mainCamera;
+    private bool _isActive;
+
+    public Action OnGameOver;
+
+    private Vector3 resetPosition;
+
+    public void Awake()
+    {
+        _mainCamera = Camera.main;
+        resetPosition = transform.position;
+    }
+
+    public void Update()
+    {
+        if (_isActive)
+        {
+            if (_mainCamera != null)
+            {
+                var t = transform.eulerAngles;
+                transform.LookAt(_mainCamera.transform);
+                transform.eulerAngles = new Vector3(t.x, transform.eulerAngles.y, t.z);
+            }
+
+            //moving
+
+            if (Vector3.Distance(transform.position, _targetTransform.position) > 2)
+            {
+                transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+            }
+            else
+            {
+                OnGameOver?.Invoke();
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        transform.position = resetPosition;
+        _isActive = true;
+        _audioSource.Play();
+    }
+
+    public void Reset()
+    {
+        _isActive = false;
+        transform.position = resetPosition;
+    }
+}
